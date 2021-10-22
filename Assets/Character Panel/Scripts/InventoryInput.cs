fe148b29408e1fd48d10d1cab6226d1fce72f3bb
turmoil2k <juma1112009@hotmail.com>
2021-10-22 21:11:02 +0400

    inventory now opens & closes in respect with the mouse cursor.

--- Assets/Character Panel/Scripts/InventoryInput.cs
+++ Assets/Character Panel/Scripts/InventoryInput.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject inventoryGameObject;
+    [SerializeField] KeyCode[] toggleInventoryKeys;
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < toggleInventoryKeys.Length; i++)
+        {
+            if (Input.GetKeyDown(toggleInventoryKeys[i]))
+            {
+                inventoryGameObject.SetActive(!inventoryGameObject.activeSelf);
+                if (inventoryGameObject.activeSelf)
+                {
+                    ShowMouseCursor();
+                }
+                else
+                {
+                    HideMouseCursor();
+                }
+                break;
+            }
+        }
+    }
+
+    public void ShowMouseCursor()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void HideMouseCursor()
+    {
+        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Confined;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
