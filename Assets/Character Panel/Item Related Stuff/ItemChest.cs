291242c698ff0caf8dab2601bf004c5b156734a3
turmoil2k <juma1112009@hotmail.com>
2021-10-22 23:56:14 +0400

    item pick up testing done with new inven system

---
+++ Assets/Character Panel/Item Related Stuff/ItemChest.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemChest : MonoBehaviour
+{
+    [SerializeField] Item item;
+    [SerializeField] Inventory inventory;
+    [SerializeField] KeyCode itemPickup = KeyCode.E;
+
+    bool isInRange = false;
+    bool isEmpty = false;       // dont need this can just null the item but if you dont want to lose the reference do this way
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        if (inventory == null)
+        {
+            inventory = FindObjectOfType<Inventory>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isEmpty && isInRange && Input.GetKeyDown(itemPickup))
+        {
+            inventory.AddItem(item);
+            isEmpty = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        InRangeTriggerCheck(other.gameObject, true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        InRangeTriggerCheck(other.gameObject, false);
+    }
+
+    void InRangeTriggerCheck(GameObject gameObject, bool state)
+    {
+        if (gameObject.CompareTag("Player"))
+        {
+            isInRange = state;
+            //enabeled = state
+        }
+    }    
+}
