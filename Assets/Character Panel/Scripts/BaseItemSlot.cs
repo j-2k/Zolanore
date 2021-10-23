4bb36d8c28bda55cd44d6c31ba3102503138e09b
turmoil2k <juma1112009@hotmail.com>
2021-10-24 03:52:29 +0400

    fixed a small bug with inventory not being able to pick an item if the inventory was not enabled at start?? kind of a bad ish way to fix but i think its very good for now

--- Assets/Character Panel/Scripts/BaseItemSlot.cs
+++ Assets/Character Panel/Scripts/BaseItemSlot.cs
@@ -2,11 +2,12 @@ using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class BaseItemSlot : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
 	[SerializeField] protected Image image;
-	[SerializeField] protected Text amountText;
+	[SerializeField] protected TextMeshProUGUI amountText;
 
 	public event Action<BaseItemSlot> OnPointerEnterEvent;
 	public event Action<BaseItemSlot> OnPointerExitEvent;
@@ -82,7 +83,7 @@ public class BaseItemSlot : MonoBehaviour, IPointerClickHandler, IPointerEnterHa
 			image = GetComponent<Image>();
 
 		if (amountText == null)
-			amountText = GetComponentInChildren<Text>();
+			amountText = GetComponentInChildren<TextMeshProUGUI>();
 
 		Item = _item;
 		Amount = _amount;
--- Assets/Character Panel/Scripts/InventoryInput.cs
+++ Assets/Character Panel/Scripts/InventoryInput.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryInput : MonoBehaviour
@@ -7,6 +10,10 @@ public class InventoryInput : MonoBehaviour
     [SerializeField] KeyCode toggleEquipmentPanelKey;
     [SerializeField] GameObject equipmentPanelGameObject;
 
+    private void Start()
+    {
+        StartCoroutine(InventoryFixEarly());
+    }
 
     // Update is called once per frame
     void Update()
@@ -57,6 +64,15 @@ public class InventoryInput : MonoBehaviour
     public void ToggleEquipmentPanel()
     {
         equipmentPanelGameObject.SetActive(!equipmentPanelGameObject.activeSelf);
+    }
+
+    IEnumerator InventoryFixEarly()
+    {
+        inventoryPanelGameobject.SetActive(true);
+        equipmentPanelGameObject.SetActive(true);
+        yield return new WaitForEndOfFrame();
+        inventoryPanelGameobject.SetActive(false);
+        equipmentPanelGameObject.SetActive(false);
     }
 
 }
