57c94c598d59cafc43674698013817e2c01c03b4
turmoil2k <juma1112009@hotmail.com>
2021-12-10 03:18:26 +0400

    trying to do glow effect

--- Assets/MainGame/Zolanore UI/ChestPromptUI.cs
+++ Assets/MainGame/Zolanore UI/ChestPromptUI.cs
@@ -7,7 +7,7 @@ public class ChestPromptUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.parent.parent.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
