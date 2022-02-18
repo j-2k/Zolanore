5a0fa0d64f66e80c60c4f364c15ea14bbf49a5ff
turmoil2k <juma1112009@hotmail.com>
2022-02-18 11:01:05 +0400

    Trying to implement animation rigging into player

--- Assets/MainGame/AbilitySystem/Base Abilities/AOEAbility.cs
+++ Assets/MainGame/AbilitySystem/Base Abilities/AOEAbility.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 [CreateAssetMenu(menuName = "Abilities/AOEAbility")]
 public class AOEAbility : Ability
@@ -9,12 +10,19 @@ public class AOEAbility : Ability
     Transform meshTransform;
     CharacterController cc;
     float timer = 0;
+    Rig armRig;
+    RigBuilder rigController;
 
     public override void CacheStart(GameObject parent,GameObject gameManagerObj)
     {
         combatType = CombatType.Melee;
         player = parent.GetComponent<PlayerManager>();
         meshTransform = parent.transform.GetChild(0);
+        Transform last =  meshTransform.GetChild(meshTransform.childCount-1);
+        armRig = last.GetComponent<Rig>();
+        armRig.weight = 0;
+        rigController = meshTransform.GetComponent<RigBuilder>();
+        rigController.enabled = false;
         cc = parent.GetComponent<CharacterController>();
     }
 
@@ -22,6 +30,8 @@ public class AOEAbility : Ability
     {
         timer = 0;
         player.isMovingAbility = true;
+        armRig.weight = 1;
+        rigController.enabled = true;
     }
 
     public override void AbilityUpdateActive(GameObject parent)
@@ -42,6 +52,8 @@ public class AOEAbility : Ability
     {
         player.isMovingAbility = false;
         meshTransform.transform.localRotation = Quaternion.identity;
+        armRig.weight = 0;
+        rigController.enabled = false;
     }
 
     public override void AbilityUpdateCooldown(GameObject parent)
