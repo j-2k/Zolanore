9e64b9a7a85a73947b2a5804f573fcc9862f1e36
turmoil2k <juma1112009@hotmail.com>
2022-04-09 02:29:49 +0400

    Death Added

--- Assets/MainGame/Player Folder/Player-SM/ScreenDarken.cs
+++ Assets/MainGame/Player Folder/Player-SM/ScreenDarken.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScreenDarken : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Image fadeImage;
 
-    // Update is called once per frame
+    [SerializeField] Color colToChange;
+    public float alpha;
+
+    [SerializeField] CharacterManager cm;
+
     void Update()
     {
-        
+        if (CharacterManager.isDead)
+        {
+            colToChange = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alpha);
+            fadeImage.color = colToChange;
+            if (alpha > 1)
+            {
+                alpha = 1;
+                cm.gameObject.transform.position = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+                StartCoroutine(Respawn());
+            }
+            else
+            {
+                alpha += Time.deltaTime * 0.25f;
+            }
+        }
+        else
+        {
+            colToChange = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, alpha);
+            fadeImage.color = colToChange;
+            if (alpha > 0)
+            {
+                alpha -= Time.deltaTime * 0.25f;
+            }
+            else
+            {
+                alpha = 0;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(1);
+        cm.RespawnPlayer();
     }
 }
