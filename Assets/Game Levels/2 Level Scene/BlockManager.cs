f06d4ebe5e1b67408cd85fc5962cb75e5379d038
turmoil2k <juma1112009@hotmail.com>
2022-04-21 14:13:49 +0400

    fixed both z shifts positive and negative now only x left

--- Assets/Game Levels/2 Level Scene/BlockManager.cs
+++ Assets/Game Levels/2 Level Scene/BlockManager.cs
@@ -23,40 +23,120 @@ public class BlockManager : MonoBehaviour
 
     Vector3 lastQuad;
     int arrayShift = 5;
-    int arrayStart = 0;
+    int arrayStart = 0; //0 for forward 4 for back
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(arrayStart + " Array start");
         if (player.transform.position.z >= lastQuad.z + 50)
         {
             Debug.Log("Front by 50");
+            //arrayStart = 0;
         }
 
         if (player.transform.position.z <= lastQuad.z - 50)
         {
             Debug.Log("Back by 50");
+            //arrayStart = 4;
         }
 
-        if (player.transform.position.z >= lastQuad.z + 50 || player.transform.position.z <= lastQuad.z - 50)
+        if (player.transform.position.z >= lastQuad.z + 50)
         {
-            //Debug.Log("Front by 50"); //entered a new chunk on z forward
-
-            for (int i = arrayStart; i < listOfBlocks.Count;)
-            {
-                listOfBlocks[i].transform.position = new Vector3(listOfBlocks[i].transform.position.x, listOfBlocks[i].transform.position.y, listOfBlocks[i].transform.position.z + 500);
-                i += arrayShift;
-            }
-
-            if (arrayStart >= 4)
-            {
-                arrayStart = 0;
-            }
-            else
-            {
-                arrayStart++;
-            }
-
-            lastQuad.z += 100;
+            ExecuteZforward();
         }
+
+        if (player.transform.position.z <= lastQuad.z - 50)
+        {
+            ExecuteZbackwards();
+        }
+
+        if (player.transform.position.x >= lastQuad.x + 50)
+        {
+            ExecuteXright();
+        }
+    }
+
+    void ExecuteZbackwards()
+    {
+        if (arrayStart <= 0)
+        {
+            arrayStart = 4;
+        }
+        else
+        {
+            arrayStart--;
+        }
+
+        Debug.Log("Executing Z backward Shift");
+        for (int i = arrayStart; i < listOfBlocks.Count;)
+        {
+            listOfBlocks[i].transform.position = new Vector3(listOfBlocks[i].transform.position.x, listOfBlocks[i].transform.position.y, listOfBlocks[i].transform.position.z - 500);
+            i += arrayShift;
+        }
+
+        lastQuad.z -= 100;
+    }
+
+    void ExecuteZforward()
+    {
+        Debug.Log("Executing Z Forward Shift");
+        for (int i = arrayStart; i < listOfBlocks.Count;)
+        {
+            listOfBlocks[i].transform.position = new Vector3(listOfBlocks[i].transform.position.x, listOfBlocks[i].transform.position.y, listOfBlocks[i].transform.position.z + 500);
+            i += arrayShift;
+        }
+        
+        if (arrayStart >= 4)
+        {
+            arrayStart = 0;
+        }
+        else
+        {
+            arrayStart++;
+        }
+
+        lastQuad.z += 100;
+    }
+
+    void ExecuteXleft()
+    {
+        if (arrayStart <= 0)
+        {
+            arrayStart = 4;
+        }
+        else
+        {
+            arrayStart--;
+        }
+
+        Debug.Log("Executing Z backward Shift");
+        for (int i = arrayStart; i < listOfBlocks.Count;)
+        {
+            listOfBlocks[i].transform.position = new Vector3(listOfBlocks[i].transform.position.x, listOfBlocks[i].transform.position.y, listOfBlocks[i].transform.position.z - 500);
+            i += arrayShift;
+        }
+
+        lastQuad.z -= 100;
+    }
+
+    void ExecuteXright()
+    {
+        Debug.Log("Executing X Right Shift");
+        for (int i = arrayStart; i < listOfBlocks.Count;)
+        {
+            listOfBlocks[i].transform.position = new Vector3(listOfBlocks[i].transform.position.x, listOfBlocks[i].transform.position.y, listOfBlocks[i].transform.position.z + 500);
+            i += arrayShift;
+        }
+
+        if (arrayStart >= 4)
+        {
+            arrayStart = 0;
+        }
+        else
+        {
+            arrayStart++;
+        }
+
+        lastQuad.z += 100;
     }
 }
