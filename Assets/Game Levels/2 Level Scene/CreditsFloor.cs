843b97528038b6e4b0e10f7e75e191fa16ab8e58
turmoil2k <juma1112009@hotmail.com>
2022-04-29 09:40:55 +0400

    credit floor fix

---
+++ Assets/Game Levels/2 Level Scene/CreditsFloor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsFloor : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("collided with player");
+            StartCoroutine(LateMove());
+        }
+    }
+
+    IEnumerator LateMove()
+    {
+        player.GetComponent<PlayerManager>().enabled = false;
+        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z);
+        yield return new WaitForSeconds(0.25f);
+        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, player.transform.position.z);
+        player.GetComponent<PlayerManager>().enabled = true;
+    }
+}
