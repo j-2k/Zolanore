192717b8f82e73fab528e42339574039e8b2d71c
turmoil2k <juma1112009@hotmail.com>
2022-04-29 12:29:20 +0400

    added a script to restore lost references in buttons

---
+++ Assets/Game Levels/0 Main Menu/QuestUIRefernceFixes/QuestReferenceFix.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestReferenceFix : MonoBehaviour
+{
+    [SerializeField] Button button;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(this.gameObject.name == "Accept")
+        {
+            button.onClick.AddListener(PlayerManager.instance.gameObject.GetComponent<QuestSystem>().AcceptQuest);
+        }
+
+        if(this.gameObject.name == "Close")
+        {
+            button.onClick.AddListener(GetComponentInParent<QuestWindow>().CloseWindow);
+            button.onClick.AddListener(PlayerManager.instance.gameObject.GetComponent<QuestSystem>().EnableCharacterRotation);
+        }
+
+        if(this.gameObject.name == "Claim")
+        {
+            button.onClick.AddListener(PlayerManager.instance.gameObject.GetComponent<QuestSystem>().Claim);
+            button.onClick.AddListener(PlayerManager.instance.gameObject.GetComponent<QuestSystem>().EnableCharacterRotation);
+        }
+    }
+}
